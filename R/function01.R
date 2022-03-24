1348a409cd12b8fcbf5c510b870298489d8e769c
angelikiKS <s212881@student.dtu.dk>
2022-03-24 15:08:09 +0000

    documentation of function 1 created

--- R/function01.R
+++ R/function01.R
@@ -1,3 +1,9 @@
+#' Make a random dna sequence of chosen length
+#'
+#' @param number integer. Defines the length of the sequence
+#'
+#' @return "ATGTTCGCAAAAA"
+#' @export
 make_dna <- function(number){
   dna <- sample(c("A", "T", "G", "C"), size = number, replace = TRUE)
   dna_seq <- paste0(dna, collapse = "")
